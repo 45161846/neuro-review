abf0cd3ac3821067d68376fb1190461c76e772ff
Mikhail Osipov <miosipov@edu.hse.ru>
2025-12-20 16:44:48 +0300

    test logic mess up

--- secret.py
+++
@@ -1,7 +0,0 @@
-import secrets
-import base64
-
-secret = base64.urlsafe_b64encode(secrets.token_bytes(32)).decode('utf-8')
-print(f"Ваш секрет для GitHub Webhook: {secret}")
-print("\nДобавьте эту строку в ваш .env файл:")
-print(f"GITHUB_WEBHOOK_SECRET={secret}")
